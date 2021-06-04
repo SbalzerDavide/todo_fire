72f36e0e7c0a0cbdf9e158107db76fe88dad53fb
davide sbalzer <sbalzerdavide@hotmail.it>
2021-06-04 12:45:52 +0200

    add elements

--- js/main.js
+++ js/main.js
@@ -13,7 +13,11 @@ const app = new Vue ({
         vue = this;
         window.db.collection("note").get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                vue.toDoList.push(doc.data());
+                let newItem = doc.data();
+                newItem.id = doc.id;
+
+                vue.toDoList.push(newItem);
+                console.log(doc.id);
             });
         });
         
@@ -22,6 +26,16 @@ const app = new Vue ({
     methods:{
         addItem(index){
             if (this.message.trim() != ''){
+                db.collection("note").add({
+                    item:this.message,
+                    check:'opacity-hidden',
+                })
+                .then((docRef) => {
+                    console.log("Document written with ID: ", docRef.id);
+                })
+                .catch((error) => {
+                    console.error("Error adding document: ", error);
+                });
                 this.toDoList.push({
                     item:this.message,
                     check:'opacity-hidden',
@@ -31,6 +45,12 @@ const app = new Vue ({
         },
         removeItem(index){
             this.toDoList.splice(index,1);
+            db.collection("note").doc("KT0DlXIPKOHZmpJ6kMkE").delete()
+            .then(() => {
+                console.log("Document successfully deleted!");
+            }).catch((error) => {
+                console.error("Error removing document: ", error);
+            });
         },
         done(index){
             if (this.toDoList[index].check === 'opacity-hidden'){
@@ -55,10 +75,10 @@ const app = new Vue ({
 //     console.error("Error adding document: ", error);
 // });
 
-db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        console.log(doc.data());
-    });
-});
+// db.collection("users").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data()}`);
+//         console.log(doc.data());
+//     });
+// });
   
